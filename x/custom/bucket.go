5b79327c470892fe475306dcd8ac7f5e1876df46
Orkun Külçe <kulceorkun@gmail.com>
2019-08-16 17:36:33 +0300

    Remove GetTimedState method

--- x/custom/bucket.go
+++ x/custom/bucket.go
@@ -1,8 +1,6 @@
 package custom
 
 import (
-	"github.com/iov-one/weave"
-	"github.com/iov-one/weave/errors"
 	"github.com/iov-one/weave/migration"
 	"github.com/iov-one/weave/orm"
 )
@@ -16,16 +14,6 @@ func NewTimedStateBucket() *TimedStateBucket {
 	return &TimedStateBucket{
 		ModelBucket: migration.NewModelBucket(packageName, b),
 	}
-}
-
-// GetTimedState loads the TimedState for the given id. If it does not exist then ErrNotFound is returned.
-func (b *TimedStateBucket) GetTimedState(db weave.KVStore, id []byte) (*TimedState, error) {
-	var timedState TimedState
-	err := b.One(db, id, &timedState)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to load timed state")
-	}
-	return &timedState, nil
 }
 
 type StateBucket struct {
